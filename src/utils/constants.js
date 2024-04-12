db147d1ee63e4917b128d122d92e5dbadf89098a
namlato102 <namdang2798@gmail.com>
2024-04-12 16:43:05 +0200

    update whitelist domains p2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // config/cors always allow localhost when in dev mode
   // 'http://localhost:5173'
-  'https://trello-web-tau-red.vercel.app/'
+  'https://trello-web-tau-red.vercel.app'
 ]
 
 export const BOARD_TYPES = {
