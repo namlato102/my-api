0726a89ed921ce391f7fde060508c4dc9e187ee9
namlato102 <namdang2798@gmail.com>
2024-07-18 20:10:12 +0200

    [fixed] white list domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ import { env } from '~/config/environment'
 export const WHITELIST_DOMAINS = [
   // config/cors always allow localhost when in dev mode
   // 'http://localhost:5173'
-  'https://trello-web-tau-red.vercel.app'
+  'https://trello-web-khaki.vercel.app'
 ]
 
 export const BOARD_TYPES = {
